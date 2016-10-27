85490257dd27aad7fa1479aee58cbd4868a53972
cokeito <coke@mta.cl>
2016-10-27 20:19:21 -0300

    gemfile updated, models created, validation of price and premium

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates_presence_of :price
+  before_validation :set_default_premium
+
+  def set_default_premium
+  	if premium.nil? 
+  		self.premium = false
+  	end
+  end
 end
